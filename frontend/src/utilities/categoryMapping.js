5141393e7e1263193f252607d156f4e58c3b4785
gdjohn4s <giovanni@exifly.it>
2023-04-17 17:06:28 +0200

    mapped all icon categories

---
+++ frontend/src/utilities/categoryMapping.js
@@ -0,0 +1,206 @@
+export const categoriesProperties = [
+  {
+    name: "Animals",
+    icon: "fa-solid fa-paw",
+  },
+  {
+    name: "Anime",
+    icon: "fa-solid fa-film",
+  },
+  {
+    name: "Anti-Malware",
+    icon: "fa-solid fa-virus-slash",
+  },
+  {
+    name: "Art & Design",
+    icon: "fa-solid fa-paintbrush",
+  },
+  {
+    name: "Authentication & Authorization",
+    icon: "fa-solid fa-unlock-keyhole",
+  },
+  {
+    name: "Blockchain",
+    icon: "fa-solid fa-bitcoin-sign",
+  },
+  {
+    name: "Books",
+    icon: "fa-solid fa-book",
+  },
+  {
+    name: "Business",
+    icon: "fa-solid fa-money-bill",
+  },
+  {
+    name: "Calendar",
+    icon: "fa-solid fa-calendar",
+  },
+  {
+    name: "Cloud Storage & File Sharing",
+    icon: "fa-solid fa-cloud",
+  },
+  {
+    name: "Continuous Integration",
+    icon: "fa-solid fa-wrench",
+  },
+  {
+    name: "Cryptocurrency",
+    icon: "fa-brands fa-ethereum",
+  },
+  {
+    name: "Currency Exchange",
+    icon: "fa-solid fa-money-bill-trend-up",
+  },
+  {
+    name: "Data Validation",
+    icon: "fa-solid fa-square-check",
+  },
+  {
+    name: "Development",
+    icon: "fa-solid fa-code",
+  },
+  {
+    name: "Dictionaries",
+    icon: "fa-solid fa-spell-check",
+  },
+  {
+    name: "Documents & Productivity",
+    icon: "fa-regular fa-folder-open",
+  },
+  {
+    name: "Email",
+    icon: "fa-solid fa-at",
+  },
+  {
+    name: "Entertainment",
+    icon: "fa-regular fa-face-laugh",
+  },
+  {
+    name: "Environment",
+    icon: "fa-solid fa-seedling",
+  },
+  {
+    name: "Events",
+    icon: "fa-solid fa-calendar-check",
+  },
+  {
+    name: "Finance",
+    icon: "fa-solid fa-coins",
+  },
+  {
+    name: "Food & Drink",
+    icon: "fa-solid fa-pizza-slice",
+  },
+  {
+    name: "Games & Comics",
+    icon: "fa-solid fa-gamepad",
+  },
+  {
+    name: "Geocoding",
+    icon: "fa-solid fa-earth-europe",
+  },
+  {
+    name: "Government",
+    icon: "fa-solid fa-building-columns",
+  },
+  {
+    name: "Health",
+    icon: "fa-solid fa-notes-medical",
+  },
+  {
+    name: "Jobs",
+    icon: "fa-solid fa-house-laptop",
+  },
+  {
+    name: "Machine Learning",
+    icon: "fa-solid fa-robot",
+  },
+  {
+    name: "Music",
+    icon: "fa-solid fa-music",
+  },
+  {
+    name: "News",
+    icon: "fa-solid fa-newspaper",
+  },
+  {
+    name: "Open Data",
+    icon: "fa-solid fa-database",
+  },
+  {
+    name: "Open Source Projects",
+    icon: "fa-brands fa-github-alt",
+  },
+  {
+    name: "Patent",
+    icon: "fa-solid fa-id-card",
+  },
+  {
+    name: "Personality",
+    icon: "fa-solid fa-circle-user",
+  },
+  {
+    name: "Phone",
+    icon: "fa-solid fa-mobile",
+  },
+  {
+    name: "Photography",
+    icon: "fa-solid fa-camera-retro",
+  },
+  {
+    name: "Programming",
+    icon: "fa-solid fa-terminal",
+  },
+  {
+    name: "Science & Math",
+    icon: "fa-solid fa-brain",
+  },
+  {
+    name: "Security",
+    icon: "fa-solid fa-shield-halved",
+  },
+  {
+    name: "Shopping",
+    icon: "fa-solid fa-bag-shopping",
+  },
+  {
+    name: "Social",
+    icon: "fa-solid fa-people-arrows",
+  },
+  {
+    name: "Sports & Fitness",
+    icon: "fa-solid fa-dumbbell",
+  },
+  {
+    name: "Test Data",
+    icon: "fa-solid fa-cube",
+  },
+  {
+    name: "Text Analysis",
+    icon: "fa-solid fa-magnifying-glass-chart",
+  },
+  {
+    name: "Tracking",
+    icon: "fa-solid fa-chart-gantt",
+  },
+  {
+    name: "Transportation",
+    icon: "fa-solid fa-shuttle-space",
+  },
+  {
+    name: "URL Shorteners",
+    icon: "fa-solid fa-sitemap",
+  },
+  {
+    name: "Vehicle",
+    icon: "fa-solid fa-car",
+  },
+  {
+    name: "Video",
+    icon: "fa-solid fa-video",
+  },
+  {
+    name: "Weather",
+    icon: "fa-solid fa-cloud-sun-rain",
+  },
+];
