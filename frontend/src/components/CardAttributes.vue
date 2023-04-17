6194518491edcb1c958325ede7295b6d379b615c
gdjohn4s <giovanni@exifly.it>
2023-04-17 14:28:45 +0200

    Component name changed

---
+++ frontend/src/components/CardAttributes.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="category-container">
+    <p>
+      <slot />
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.category-container {
+  border-radius: 4px;
+  border-color: rgb(153, 0, 0);
+  background: rgba(0, 98, 195, 0.2);
+  color: rgb(3, 155, 178);
+  margin-left: 0;
+  margin-right: auto;
+  margin-top: 10px;
+  margin-bottom: 6px;
+  justify-content: center;
+  width: 4vw;
+}
+
+p {
+  font-size: 14px;
+  margin: 0;
+  text-align: center;
+}
+
+@media only screen and (max-width: 600px) {
+  .category-container {
+    border-radius: 4px;
+    border-color: rgb(153, 0, 0);
+    background: rgba(0, 98, 195, 0.2);
+    color: rgb(3, 155, 178);
+    margin-left: 0;
+    margin-right: auto;
+    margin-top: 10px;
+    margin-bottom: 6px;
+    justify-content: center;
+    width: 20vw;
+  }
+}
+</style>
