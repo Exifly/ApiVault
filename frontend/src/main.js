4ac69e5ae4ee7949c0802cca67a807af34b3f012
gdjohn4s <giovanni@exifly.it>
2023-04-17 17:06:42 +0200

    injected categoryMapping as global variable

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,21 +11,75 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
   faAnglesRight,
+  faAt,
+  faBagShopping,
   faBars,
+  faBitcoinSign,
+  faBook,
+  faBrain,
+  faBuildingColumns,
+  faCalendar,
+  faCalendarCheck,
+  faCameraRetro,
+  faCircleUser,
+  faCloud,
+  faCode,
+  faCoins,
+  faCube,
+  faDatabase,
+  faDumbbell,
+  faEarthEurope,
+  faFilm,
+  faGamepad,
+  faHouseLaptop,
+  faIdCard,
   faListUl,
   faMagnifyingGlass,
+  faMobile,
+  faMoneyBill,
+  faMoneyBillTrendUp,
   faMoon,
+  faMusic,
+  faNewspaper,
+  faNotesMedical,
+  faPaintBrush,
+  faPaw,
+  faPeopleArrows,
+  faPizzaSlice,
+  faRobot,
+  faSeedling,
+  faShieldHalved,
+  faSpellCheck,
+  faSquareCheck,
   faSun,
+  faTerminal,
+  faUnlockKeyhole,
   faUserSecret,
+  faVirusSlash,
+  faWrench,
 } from "@fortawesome/free-solid-svg-icons";
-import { faFlag } from "@fortawesome/free-regular-svg-icons";
 import {
+  faFaceLaugh,
+  faFlag,
+  faFolderOpen,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faEthereum,
   faGitAlt,
   faGithub,
+  faGithubAlt,
   faGithubSquare,
 } from "@fortawesome/free-brands-svg-icons";
 
 import router from "./router/router";
+import { faMagnifyingGlassChart } from "@fortawesome/free-solid-svg-icons";
+import { faChartGantt } from "@fortawesome/free-solid-svg-icons";
+import { faShuttleSpace } from "@fortawesome/free-solid-svg-icons";
+import { faSitemap } from "@fortawesome/free-solid-svg-icons";
+import { faCar } from "@fortawesome/free-solid-svg-icons";
+import { faVideo } from "@fortawesome/free-solid-svg-icons";
+import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
+import { categoriesProperties } from "@/utilities/categoryMapping.js";
 
 library.add(
   faUserSecret,
@@ -38,15 +92,63 @@ library.add(
   faSun,
   faMagnifyingGlass,
   faMoon,
-  faBars
+  faBars,
+  faPaw,
+  faFilm,
+  faVirusSlash,
+  faPaintBrush,
+  faUnlockKeyhole,
+  faBitcoinSign,
+  faBook,
+  faMoneyBill,
+  faCalendar,
+  faCloud,
+  faWrench,
+  faEthereum,
+  faMoneyBillTrendUp,
+  faSquareCheck,
+  faCode,
+  faSpellCheck,
+  faFolderOpen,
+  faAt,
+  faFaceLaugh,
+  faSeedling,
+  faCalendarCheck,
+  faCoins,
+  faPizzaSlice,
+  faGamepad,
+  faEarthEurope,
+  faBuildingColumns,
+  faNotesMedical,
+  faHouseLaptop,
+  faRobot,
+  faMusic,
+  faNewspaper,
+  faDatabase,
+  faGithubAlt,
+  faIdCard,
+  faCircleUser,
+  faMobile,
+  faCameraRetro,
+  faTerminal,
+  faBrain,
+  faShieldHalved,
+  faBagShopping,
+  faPeopleArrows,
+  faDumbbell,
+  faCube,
+  faMagnifyingGlassChart,
+  faChartGantt,
+  faShuttleSpace,
+  faSitemap,
+  faCar,
+  faVideo,
+  faCloudSunRain
 );
-
-// global variables
-const colorScheme = ref("dark");
 
 createApp(App)
   .use(router)
   .use(bootstrap)
-  .provide("colorScheme", colorScheme)
+  .provide("categoryMapping", categoriesProperties)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
