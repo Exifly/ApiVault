aefbeb86ace57d071a2d0abd163c48a80843e636
gdjohn4s <giovanni@exifly.it>
2023-05-06 20:19:20 +0200

    added TrendCategory component

---
+++ frontend/src/components/TrendCategory.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="trend-category-container px-2">
+    <p>
+      <slot />
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.trend-category-container {
+  align-self: center;
+  border-radius: 4px;
+  border: 1px solid;
+  border-color: var(--card-attribute-color);
+  background: var(--bg-card-attribute);
+  color: var(--card-attribute-color);
+  margin-left: 0;
+  margin-right: 1vw;
+  margin-bottom: 1rem;
+  min-width: 3rem;
+  max-width: 12rem;
+  justify-content: center;
+}
+
+.trend-category-container p {
+  font-size: 16px;
+}
+
+.trend-category-container:hover {
+  background-color: var(--bg-card-glass-hover);
+}
+
+p {
+  font-size: 12px;
+  font-weight: 600;
+  margin: 0;
+  text-align: center;
+}
+
+@media only screen and (max-width: 600px) {
+  .trend-category-container {
+    border-radius: 4px;
+    border: 1px solid;
+    border-color: var(--card-attribute-color);
+    background: var(--bg-card-attribute);
+    color: var(--card-attribute-color);
+    margin-left: 0;
+    margin-right: 4vw;
+    margin-top: 10px;
+    margin-bottom: 6px;
+    justify-content: center;
+    width: 20vw;
+  }
+}
+</style>
