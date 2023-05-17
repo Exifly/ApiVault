1a9bd22f897ebb26efca44ef2ede5a36f87835f4
Igweokolo Davidson <davidsonigweokolo@gmail.com>
2023-05-17 11:54:35 +0100

    i updated the code using composition API

--- frontend/src/components/ToggleButton.vue
+++ frontend/src/components/ToggleButton.vue
@@ -1,60 +1,69 @@
 <template>
-    <button class="toggle-button" :class="{ active: isActive }" @click="toggle">
-      <span class="toggle-icon">
-        <font-awesome-icon :icon="isActive ? 'sun' : 'moon'" />
-      </span>
-    </button>
-  </template>
-  
-  <script>
-  import { library } from '@fortawesome/fontawesome-svg-core';
-  import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
-  
-  library.add(faSun, faMoon);
-  export default {
-    data() {
-      return {
-        isActive: false
-      };
-    },
-    methods: {
-      toggle() {
-        this.isActive = !this.isActive;
-      }
-    }
-  };
-  </script>
-  
-  <style>
-  .toggle-button {
-    width: 60px;
-    height: 30px;
-    background-color: #ccc;
-    border-radius: 15px;
-    position: relative;
-    outline: none;
-    border: none;
-    cursor: pointer;
-  }
-  
-  .toggle-button.active {
-    background-color: black;
-  }
-  
-  .toggle-button.active .toggle-icon {
-    transform: translateX(30px);
-  }
-  
-  .toggle-icon {
-    display: flex;
-    position: absolute;
-    left: 0;
-    justify-content: center;
-    align-items: center;
-    width: 30px;
-    height: 30px;
-    background-color: #fff;
-    border-radius: 50%;
-    transition: transform 0.3s ease;
-  }
-  </style>
+  <button class="toggle-button" :class="{ active: isActive }" @click="toggle">
+    <span class="toggle-icon">
+      <font-awesome-icon :icon="icon" />
+    </span>
+  </button>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSun, faMoon);
+
+export default {
+  setup() {
+    const isActive = ref(false);
+
+    const toggle = () => {
+      isActive.value = !isActive.value;
+    };
+
+    const icon = computed(() => {
+      return isActive.value ? "sun" : "moon";
+    });
+
+    return {
+      isActive,
+      toggle,
+      icon,
+    };
+  },
+};
+</script>
+
+<style>
+.toggle-button {
+  width: 60px;
+  height: 30px;
+  background-color: #ccc;
+  border-radius: 15px;
+  position: relative;
+  outline: none;
+  border: none;
+  cursor: pointer;
+}
+
+.toggle-button.active {
+  background-color: black;
+}
+
+.toggle-button.active .toggle-icon {
+  transform: translateX(30px);
+}
+
+.toggle-icon {
+  display: flex;
+  position: absolute;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  background-color: #fff;
+  border-radius: 50%;
+  transition: transform 0.3s ease;
+}
+</style>
