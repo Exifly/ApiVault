c2e1696d7a6d74ca066513ed83ddef648a596248
gdjohn4s <giovanni@exifly.it>
2023-05-22 15:36:37 +0200

    added cleanup function

--- frontend/docker/start_server.py
+++ frontend/docker/start_server.py
@@ -1,6 +1,14 @@
 import subprocess
 import os
 
+def cleanup() -> None:
+    file_to_remove = "/tmp/nitro/worker-*.sock"
+    result = subprocess.call(f"rm -f {file_to_remove}", shell=True)
+    print("Done") if result == 0 else print("Failed to remove file .sock")
+
+# Cleanup
+cleanup()
+
 PS_YARN_INSTALL = "yarn install"
 PS_YARN_RUN_DEV = "yarn dev"
 
