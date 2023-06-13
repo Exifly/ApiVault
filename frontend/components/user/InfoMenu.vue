9d83a31aeba7168bbdec9a7ec6ce85851a4f9700
gdjohn4s <giovanni@exifly.it>
2023-06-13 12:40:21 +0200

    feat/added user info navbar component

---
+++ frontend/components/user/InfoMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="user-info-container">
+    <p class="text-wrapper">Img</p>
+    <p class="text-wrapper">John Doe</p>
+  </section>
+</template>
+
+<style scoped>
+.user-info-container {
+  display: flex;
+  flex-direction: row;
+  gap: 2px;
+}
+
+.user-info-container .text-wrapper {
+  margin: 0 !important;
+}
+</style>
