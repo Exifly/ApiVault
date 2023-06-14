8ed8ba9cf641b3a7921d831fbcfb06ce9fcb7405
gdjohn4s <giovanni@exifly.it>
2023-06-14 18:44:24 +0200

    added username prop into user component

--- frontend/components/user/InfoMenu.vue
+++ frontend/components/user/InfoMenu.vue
@@ -2,12 +2,21 @@
   <section class="user-info-container">
     <GenericsButton>
       <p class="text-wrapper">
-        John Doe
+        {{ username }}
         <font-awesome-icon class="ms-2" :icon="['fas', 'right-from-bracket']" />
       </p>
     </GenericsButton>
   </section>
 </template>
+
+<script lang="ts" setup>
+defineProps({
+  username: {
+    type: String,
+    required: true,
+  },
+});
+</script>
 
 <style scoped>
 .user-info-container {
