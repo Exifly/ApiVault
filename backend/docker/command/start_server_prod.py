382f751314920051b484c7ca04a9aca87e80759e
gdjohn4s <giovanni@exifly.it>
2023-09-01 18:46:19 +0200

    added collectstatics with --noinput flag

--- backend/docker/command/start_server_prod.py
+++ backend/docker/command/start_server_prod.py
@@ -13,7 +13,7 @@ PYTHON_COMMAND = "python3" if result_code != 0 else "python"
 CMD_MAKE_MIGRATIONS: str = f"{PYTHON_COMMAND} manage.py makemigrations"
 CMD_MIGRATE_AUTH: str = f"{PYTHON_COMMAND} manage.py migrate auth"
 CMD_MIGRATE: str = f"{PYTHON_COMMAND} manage.py migrate"
-CMD_STATIC: str = f"{PYTHON_COMMAND} manage.py collectstatic"
+CMD_STATIC: str = f"{PYTHON_COMMAND} manage.py collectstatic --noinput"
 
 CMD_LOAD_APIS: str = f"{PYTHON_COMMAND} manage.py loaddata fixtures/apis.json --app vault"
 CMD_LOAD_CATEGORIES: str = f"{PYTHON_COMMAND} manage.py loaddata fixtures/categories.json --app vault"
@@ -28,5 +28,5 @@ subprocess.call(CMD_MIGRATE, shell=True)
 subprocess.call(CMD_LOAD_CATEGORIES, shell=True)
 subprocess.call(CMD_LOAD_APIS, shell=True)
 
-#subprocess.call(CMD_STATIC, shell=True)
+subprocess.call(CMD_STATIC, shell=True)
 subprocess.call(CMD_RUNSERVER, shell=True)
