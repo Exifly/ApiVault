4e479351fa8cfcee777d853d576bdc0a6f5dfdf7
gdjohn4s <giovanni@exifly.it>
2023-09-14 00:44:24 +0200

    created product hunt badge

---
+++ frontend/components/ProductHuntBadge.vue
@@ -0,0 +1,12 @@
+<template>
+  <a
+    href="https://www.producthunt.com/posts/apivault?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-apivault"
+    target="_blank"
+    ><img
+      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=414441&theme=dark"
+      alt="Apivault - Your&#0032;gateway&#0032;to&#0032;a&#0032;world&#0032;of&#0032;public&#0032;APIs&#0046; | Product Hunt"
+      style="width: 250px; height: 54px"
+      width="250"
+      height="54"
+  /></a>
+</template>
