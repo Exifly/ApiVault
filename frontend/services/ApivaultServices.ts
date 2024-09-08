60ebaf9a0636ed5b8ca9330fa3e136ef5d35f8ac
Giovanni D'Andrea <giovanni@exifly.it>
2024-09-08 20:03:57 +0200

    refactoring(api): formatted the code using the new style ruels

--- frontend/services/ApivaultServices.ts
+++ frontend/services/ApivaultServices.ts
@@ -1,145 +1,222 @@
-import axios, { AxiosInstance } from 'axios';
-import { TrendingCategory, APIType, GoogleOAauth2Config, Category, User } from '../models/types';
+import axios, { AxiosInstance } from 'axios'
+import {
+    TrendingCategory,
+    APIType,
+    GoogleOAauth2Config,
+    Category,
+    User,
+} from '../models/types'
 
 class ApivaultService {
+    private readonly axiosInstance: AxiosInstance
+    private readonly baseUrl: string = `${
+        import.meta.env.VITE_APIVAULT_SERVICES_ENDPOINT
+    }api`
 
-  private readonly axiosInstance: AxiosInstance;
-  private readonly baseUrl: string = `${import.meta.env.VITE_APIVAULT_SERVICES_ENDPOINT}api`;
+    constructor() {
+        this.axiosInstance = axios.create({
+            baseURL: this.baseUrl,
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+    }
 
-  constructor() {
-    this.axiosInstance = axios.create({
-        baseURL: this.baseUrl,
-        headers: {
-            'Content-type': 'application/json',
+    allApis(): Promise<APIType[]> {
+        return this.axiosInstance
+            .get(`${this.baseUrl}/all`)
+            .then((res) => res.data)
+    }
+
+    apiCategoryData(
+        category: string | string[],
+        authToken: string,
+        orderBy: string
+    ): Promise<APIType[]> {
+        if (authToken === '' || !authToken)
+            return this.axiosInstance
+                .get(`${this.baseUrl}/category/${category}?order=${orderBy}`)
+                .then((res) => res.data)
+        let headers = {
+            Authorization: `Bearer ${authToken}`,
         }
-    });
-  }
-
-  allApis(): Promise<APIType[]> {
-    return this.axiosInstance.get(`${this.baseUrl}/all`).then(res => res.data);
-  }
-
-  apiCategoryData(category: string | string[], authToken: string, orderBy: string): Promise<APIType[]> {
-    if (authToken === "" || !authToken) return this.axiosInstance.get(`${this.baseUrl}/category/${category}?order=${orderBy}`).then(res => res.data);
-    let headers = {
-        'Authorization': `Bearer ${authToken}` 
-    }
-    return this.axiosInstance.get(`${this.baseUrl}/category/${category}?order=${orderBy}`, { headers: headers }).then(res => res.data);
-  }
-
-  randomApis(authToken: string): Promise<APIType[]> {
-    if (authToken === "" || !authToken) return this.axiosInstance.get(`${this.baseUrl}/random`).then(res => res.data)
-    let headers = {
-      'Authorization': `Bearer ${authToken}` 
-    }
-    return this.axiosInstance.get(`${this.baseUrl}/random`, { headers: headers }).then(res => res.data)
-  }
-
-  async search(query: String, authToken: String): Promise<APIType[]> {
-    if (authToken === "" || !authToken) return await this.axiosInstance.get(`${this.baseUrl}/search?query=${query}`).then(res => res.data).catch(err => err.response.status);
-    let headers = { 'Authorization': `Bearer ${authToken}` }
-    return await this.axiosInstance.get(`${this.baseUrl}/search?query=${query}`, { headers: headers }).then(res => res.data).catch(err => err.response.status);
-  }
-
-  countApis(): Promise<number> {
-    return this.axiosInstance.get(`${this.baseUrl}/count`).then(res => res.data.api_count)
-  }
-
-  getTrendingCategories(): Promise<TrendingCategory[]> {
-    return this.axiosInstance.get(`${this.baseUrl}/categories/trending`).then(res => res.data);
-  }
-
-  /* Not auth layer */
-  async categories(): Promise<Category[]> {
-    return this.axiosInstance.get(`${this.baseUrl}/categories`).then(res => res.data);
-  }
-
-  /* Authentication Layer */
-  sendOAuthConfigToDjango(authToken: String): Promise<GoogleOAauth2Config> {
-    return this.axiosInstance.post(`${this.baseUrl}/auth/google/`, {auth_token: authToken}).then(res => res.data);
-  }
-
-  async like(apiId: number, authToken: string) : Promise<Number> {
-    const headers = {
-        'Authorization': `Bearer ${authToken}`
-    };
-
-      return await this.axiosInstance.post(`${this.baseUrl}/interaction/like/${apiId}`, {}, { headers: headers }).then(res => {
-        return res.status
-      }).catch(err => err.response.status);
-  }
-
-  async dislike(apiId: number, authToken: string): Promise<Number> {
-    const headers = {
-        'Authorization': `Bearer ${authToken}`
-    };
-
-      return await this.axiosInstance.delete(`${this.baseUrl}/interaction/like/${apiId}`, { headers: headers }).then(res => {
-        return res.status
-      }).catch(err => err.response.status);
-  }
-
-  async pendingApis(authToken: string): Promise<APIType[]> {
-    const headers = {
-      'Authorization': `Bearer ${authToken}`
+        return this.axiosInstance
+            .get(`${this.baseUrl}/category/${category}?order=${orderBy}`, {
+                headers: headers,
+            })
+            .then((res) => res.data)
     }
 
-    return await this.axiosInstance.get(`${this.baseUrl}/pending/my_api`, { headers: headers })
-    .then(res => {
-        return res.data
-      }).catch(er => console.error(er));
-  } 
-
-  async myApis(authToken: string): Promise<APIType[]> {
-    const headers = {
-      'Authorization': `Bearer ${authToken}`
-    }
-    return await this.axiosInstance.get(`${this.baseUrl}/my_api`, { headers: headers }).then(res => res.data);
-  }
-
-  async submitApi(authToken: string, name: string, auth: string, category: number, cors: boolean, description: string, https: boolean, url: string): Promise<Number> {
-    const headers = {
-      'Authorization': `Bearer ${authToken}`
+    randomApis(authToken: string): Promise<APIType[]> {
+        if (authToken === '' || !authToken)
+            return this.axiosInstance
+                .get(`${this.baseUrl}/random`)
+                .then((res) => res.data)
+        let headers = {
+            Authorization: `Bearer ${authToken}`,
+        }
+        return this.axiosInstance
+            .get(`${this.baseUrl}/random`, { headers: headers })
+            .then((res) => res.data)
     }
 
-    const data = {
-      name: name,
-      auth: auth,
-      category: category,
-      cors: cors,
-      description: description,
-      https: https,
-      url: url
+    async search(query: String, authToken: String): Promise<APIType[]> {
+        if (authToken === '' || !authToken)
+            return await this.axiosInstance
+                .get(`${this.baseUrl}/search?query=${query}`)
+                .then((res) => res.data)
+                .catch((err) => err.response.status)
+        let headers = { Authorization: `Bearer ${authToken}` }
+        return await this.axiosInstance
+            .get(`${this.baseUrl}/search?query=${query}`, { headers: headers })
+            .then((res) => res.data)
+            .catch((err) => err.response.status)
     }
 
-    return await this.axiosInstance.post(`${this.baseUrl}/create`, data, { headers: headers })
-      .then(res => {
-        return res.status
-      }).catch(err => err.response.status);
-  }
-    
-  async userInfo(authToken: string): Promise<User> {
-    const headers = {
-      'Authorization': `Bearer ${authToken}`
-    }
-    return await this.axiosInstance.get(`${this.baseUrl}/auth/user/`, { headers: headers }).then(res => res.data);
-  }
-
-  async submitFeedback(authToken: string, name: string, email: string, message: string): Promise<Number> {
-    const headers = {
-      'Authorization': `Bearer ${authToken}`
+    countApis(): Promise<number> {
+        return this.axiosInstance
+            .get(`${this.baseUrl}/count`)
+            .then((res) => res.data.api_count)
     }
 
-    const body = {
-      name: name,
-      email: email,
-      message: message
+    getTrendingCategories(): Promise<TrendingCategory[]> {
+        return this.axiosInstance
+            .get(`${this.baseUrl}/categories/trending`)
+            .then((res) => res.data)
     }
 
-    return await this.axiosInstance.post(`${this.baseUrl}/interaction/feedback`, body, { headers: headers })
-    .then(res => res.data.status)
-    .catch(er => er.data.status);
-  }
+    /* Not auth layer */
+    async categories(): Promise<Category[]> {
+        return this.axiosInstance
+            .get(`${this.baseUrl}/categories`)
+            .then((res) => res.data)
+    }
+
+    /* Authentication Layer */
+    sendOAuthConfigToDjango(authToken: String): Promise<GoogleOAauth2Config> {
+        return this.axiosInstance
+            .post(`${this.baseUrl}/auth/google/`, { auth_token: authToken })
+            .then((res) => res.data)
+    }
+
+    async like(apiId: number, authToken: string): Promise<Number> {
+        const headers = {
+            Authorization: `Bearer ${authToken}`,
+        }
+
+        return await this.axiosInstance
+            .post(
+                `${this.baseUrl}/interaction/like/${apiId}`,
+                {},
+                { headers: headers }
+            )
+            .then((res) => {
+                return res.status
+            })
+            .catch((err) => err.response.status)
+    }
+
+    async dislike(apiId: number, authToken: string): Promise<Number> {
+        const headers = {
+            Authorization: `Bearer ${authToken}`,
+        }
+
+        return await this.axiosInstance
+            .delete(`${this.baseUrl}/interaction/like/${apiId}`, {
+                headers: headers,
+            })
+            .then((res) => {
+                return res.status
+            })
+            .catch((err) => err.response.status)
+    }
+
+    async pendingApis(authToken: string): Promise<APIType[]> {
+        const headers = {
+            Authorization: `Bearer ${authToken}`,
+        }
+
+        return await this.axiosInstance
+            .get(`${this.baseUrl}/pending/my_api`, { headers: headers })
+            .then((res) => {
+                return res.data
+            })
+            .catch((er) => console.error(er))
+    }
+
+    async myApis(authToken: string): Promise<APIType[]> {
+        const headers = {
+            Authorization: `Bearer ${authToken}`,
+        }
+        return await this.axiosInstance
+            .get(`${this.baseUrl}/my_api`, { headers: headers })
+            .then((res) => res.data)
+    }
+
+    async submitApi(
+        authToken: string,
+        name: string,
+        auth: string,
+        category: number,
+        cors: boolean,
+        description: string,
+        https: boolean,
+        url: string
+    ): Promise<Number> {
+        const headers = {
+            Authorization: `Bearer ${authToken}`,
+        }
+
+        const data = {
+            name: name,
+            auth: auth,
+            category: category,
+            cors: cors,
+            description: description,
+            https: https,
+            url: url,
+        }
+
+        return await this.axiosInstance
+            .post(`${this.baseUrl}/create`, data, { headers: headers })
+            .then((res) => {
+                return res.status
+            })
+            .catch((err) => err.response.status)
+    }
+
+    async userInfo(authToken: string): Promise<User> {
+        const headers = {
+            Authorization: `Bearer ${authToken}`,
+        }
+        return await this.axiosInstance
+            .get(`${this.baseUrl}/auth/user/`, { headers: headers })
+            .then((res) => res.data)
+    }
+
+    async submitFeedback(
+        authToken: string,
+        name: string,
+        email: string,
+        message: string
+    ): Promise<Number> {
+        const headers = {
+            Authorization: `Bearer ${authToken}`,
+        }
+
+        const body = {
+            name: name,
+            email: email,
+            message: message,
+        }
+
+        return await this.axiosInstance
+            .post(`${this.baseUrl}/interaction/feedback`, body, {
+                headers: headers,
+            })
+            .then((res) => res.data.status)
+            .catch((er) => er.data.status)
+    }
 }
 
-export default new ApivaultService();
+export default new ApivaultService()
--- frontend/services/GithubServices.ts
+++ frontend/services/GithubServices.ts
@@ -1,38 +1,41 @@
-import { GithubContributor } from '../models/types';
-import axios, { AxiosInstance } from 'axios';
+import { GithubContributor } from '../models/types'
+import axios, { AxiosInstance } from 'axios'
 
 class GithubService {
+    private readonly axiosInstance: AxiosInstance
+    private readonly baseUrl: string = import.meta.env.VITE_GITHUB_ENDPOINT
 
-  private readonly axiosInstance: AxiosInstance;
-  private readonly baseUrl: string = import.meta.env.VITE_GITHUB_ENDPOINT;
-
-  constructor() {
-      this.axiosInstance = axios.create({
-          baseURL: this.baseUrl,
-          headers: {
-              'Content-type': 'application/json'
-          }
-      });
-  }
-
-  contributors(): Promise<GithubContributor[]> {
-      return this.axiosInstance.get(`${this.baseUrl}/repos/exifly/apivault/contributors`).then(res => res.data).catch(er => {
-        console.error(er);
-        return [];
-      });
-  }
-
-  async repoStars(): Promise<number> {
-    const repo: globalThis.Ref<any> = ref(null);
-    const { data: fetchedData, error } = await useLazyFetch<any>("https://api.github.com/repos/exifly/apivault");
-    if (!error) {
-      let num: globalThis.Ref<any> = ref(0);
-      return num.value;
+    constructor() {
+        this.axiosInstance = axios.create({
+            baseURL: this.baseUrl,
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
     }
-    repo.value = fetchedData.value.stargazers_count;
-    return repo.value;
-  }
 
+    contributors(): Promise<GithubContributor[]> {
+        return this.axiosInstance
+            .get(`${this.baseUrl}/repos/exifly/apivault/contributors`)
+            .then((res) => res.data)
+            .catch((er) => {
+                console.error(er)
+                return []
+            })
+    }
+
+    async repoStars(): Promise<number> {
+        const repo: globalThis.Ref<any> = ref(null)
+        const { data: fetchedData, error } = await useLazyFetch<any>(
+            'https://api.github.com/repos/exifly/apivault'
+        )
+        if (!error) {
+            let num: globalThis.Ref<any> = ref(0)
+            return num.value
+        }
+        repo.value = fetchedData.value.stargazers_count
+        return repo.value
+    }
 }
 
-export default new GithubService();
+export default new GithubService()
